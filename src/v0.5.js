383f0afeaea8623eb45d637da683d5b81b84b815
dheepakg <sslvgdek@gmail.com>
2025-04-03 10:57:23 +0530

    half removed hardcoded values

--- src/v0.5.js
+++ src/v0.5.js
@@ -4,11 +4,9 @@ var svg = d3.select(".canvas").append("svg").attr("width", 1500).attr("height", 
 dimensions  =   { 'width': 1350, 'height':3100,
                     'origin': {'x': 30, 'y': 50 },
                     'over_rect': {'height':30, 'width': 30, 'curve':10, 'offset':6 },
-                    'match_rect': {'x':630, 'y': 50+50, 'offset':2 , 'width':100, 'height':40}
+                    'match_rect': {'x':630, 'y': 50+50, 'offset':2 ,'spacing':10 , 'width':100, 'height':40}
                 };
-// origin = { 'x': 30, 'y': 50 };
-match_rect = {'x':630, 'y': 50+50, 'offset':2 , 'width':100, 'height':40};
-over_rect = {'height':30, 'width': 30, 'curve':10, 'offset':6 };
+                
 
 const graph = svg.append("g").attr("width", dimensions.width+100).attr("height", dimensions.height);
 
@@ -115,13 +113,13 @@ graph.append("text")
     .attr("font-size", "10px");
 
 graph.append("rect")
-    .attr('x',90 )
-    .attr('y',30 )
-    .attr('width',over_rect.width * 0.8)
-    .attr('height', match_rect.height * 0.8)
-    .attr('rx',over_rect.curve * 0.8)
-    .attr('ry',over_rect.curve * 0.8)
-    .attr("fill", runScoredColor(1) )
+    .attr('x',      90 )
+    .attr('y',      30 )
+    .attr('width',  dimensions.over_rect.width * 0.8)
+    .attr('height', dimensions.match_rect.height * 0.8)
+    .attr('rx',     dimensions.over_rect.curve * 0.8)
+    .attr('ry',     dimensions.over_rect.curve * 0.8)
+    .attr("fill",   runScoredColor(1) )
     .attr('stroke', '#111')
 
 graph.append("text")
@@ -132,68 +130,68 @@ graph.append("text")
     .attr("font-size", "10px");
 
 graph.append("rect")
-    .attr('x',90 + over_rect.width + over_rect.offset )
+    .attr('x',90 + dimensions.over_rect.width + dimensions.over_rect.offset )
     .attr('y',30 )
-    .attr('width',over_rect.width * 0.8)
-    .attr('height', match_rect.height * 0.8)
-    .attr('rx',over_rect.curve * 0.8)
-    .attr('ry',over_rect.curve * 0.8)
-    .attr("fill", runScoredColor(6) )
+    .attr('width',  dimensions.over_rect.width * 0.8)
+    .attr('height', dimensions.match_rect.height * 0.8)
+    .attr('rx',     dimensions.over_rect.curve * 0.8)
+    .attr('ry',     dimensions.over_rect.curve * 0.8)
+    .attr("fill",   runScoredColor(6) )
     .attr('stroke', '#111')
 
 graph.append("text")
-    .attr('x', 90 + (over_rect.width  + over_rect.offset) + 8 )
+    .attr('x', 90 + (dimensions.over_rect.width  + dimensions.over_rect.offset) + 8 )
     .attr('y', 50)
     .text('6')
     .attr("font-family", "monospace")
     .attr("font-size", "10px");
 
 graph.append("rect")
-    .attr('x',90 + (over_rect.width  + over_rect.offset) * 2 )
+    .attr('x',90 + (dimensions.over_rect.width  + dimensions.over_rect.offset) * 2 )
     .attr('y',30 )
-    .attr('width',over_rect.width * 0.8)
-    .attr('height', match_rect.height * 0.8)
-    .attr('rx',over_rect.curve * 0.8)
-    .attr('ry',over_rect.curve * 0.8)
-    .attr("fill", runScoredColor(12) )
+    .attr('width',  dimensions.over_rect.width * 0.8)
+    .attr('height', dimensions.match_rect.height * 0.8)
+    .attr('rx',     dimensions.over_rect.curve * 0.8)
+    .attr('ry',     dimensions.over_rect.curve * 0.8)
+    .attr("fill",   runScoredColor(12) )
     .attr('stroke', '#111')
 
 graph.append("text")
-    .attr('x', 90 + (over_rect.width  + over_rect.offset) * 2 + 8)
+    .attr('x', 90 + (dimensions.over_rect.width  + dimensions.over_rect.offset) * 2 + 8)
     .attr('y', 50)
     .text('12')
     .attr("font-family", "monospace")
     .attr("font-size", "10px");
 
 graph.append("rect")
-    .attr('x',90 + (over_rect.width  + over_rect.offset) * 3 )
+    .attr('x',90 + (dimensions.over_rect.width  + dimensions.over_rect.offset) * 3 )
     .attr('y',30 )
-    .attr('width',over_rect.width * 0.8)
-    .attr('height', match_rect.height * 0.8)
-    .attr('rx',over_rect.curve * 0.8)
-    .attr('ry',over_rect.curve * 0.8)
-    .attr("fill", runScoredColor(24) )
+    .attr('width',  dimensions.over_rect.width * 0.8)
+    .attr('height', dimensions.match_rect.height * 0.8)
+    .attr('rx',     dimensions.over_rect.curve * 0.8)
+    .attr('ry',     dimensions.over_rect.curve * 0.8)
+    .attr("fill",   runScoredColor(24) )
     .attr('stroke', '#111');
 
 graph.append("text")
-    .attr('x', 90 + (over_rect.width  + over_rect.offset) * 3  + 8)
+    .attr('x', 90 + (dimensions.over_rect.width  + dimensions.over_rect.offset) * 3  + 8)
     .attr('y', 50)
     .text('24')
     .attr("font-family", "monospace")
     .attr("font-size", "10px");
 
 graph.append("rect")
-    .attr('x',90 + (over_rect.width  + over_rect.offset) * 4 )
+    .attr('x',90 + (dimensions.over_rect.width  + dimensions.over_rect.offset) * 4 )
     .attr('y',30 )
-    .attr('width',over_rect.width * 0.8)
-    .attr('height', match_rect.height * 0.8)
-    .attr('rx',over_rect.curve * 0.8)
-    .attr('ry',over_rect.curve * 0.8)
-    .attr("fill", runScoredColor(36) )
+    .attr('width',  dimensions.over_rect.width * 0.8)
+    .attr('height', dimensions.match_rect.height * 0.8)
+    .attr('rx',     dimensions.over_rect.curve * 0.8)
+    .attr('ry',     dimensions.over_rect.curve * 0.8)
+    .attr("fill",   runScoredColor(36) )
     .attr('stroke', '#111');
 
 graph.append("text")
-    .attr('x', 90 + (over_rect.width  + over_rect.offset) * 4  + 8)
+    .attr('x', 90 + (dimensions.over_rect.width  + dimensions.over_rect.offset) * 4  + 8)
     .attr('y', 50)
     .text('36')
     .attr("font-family", "monospace")
@@ -201,8 +199,8 @@ graph.append("text")
 // End of Legends
 
 graph.append("text")
-    .attr('x', match_rect.x+10 )
-    .attr('y', match_rect.y - 10 )
+    .attr('x', dimensions.match_rect.x + dimensions.match_rect.spacing )
+    .attr('y', dimensions.match_rect.y - dimensions.match_rect.spacing )
     .text("Match Details")
     .attr("font-family", "monospace")
     .attr("font-size", "10px");
@@ -214,16 +212,16 @@ for (let over_notifier = 0; over_notifier < 20; over_notifier++) {
     if ((over_notifier +1)  ===6 || (over_notifier +1)  ===15 || (over_notifier +1)  ===20)  {
         // First innings
         graph.append("text")
-            .attr('x', match_rect.x - ((over_notifier + 1) * over_rect.width) + 10)
-            .attr('y', match_rect.y - 10)
+            .attr('x', dimensions.match_rect.x - ((over_notifier + 1) * dimensions.over_rect.width) + 10)
+            .attr('y', dimensions.match_rect.y - dimensions.match_rect.spacing)
             .text(over_notifier + 1)
             .attr("font-family", "monospace")
             .attr("font-size", "10px");
 
         // Second innings
         graph.append("text")
-            .attr('x', match_rect.x + ((over_notifier + 1) * over_rect.width) + 80)
-            .attr('y', match_rect.y - 10)
+            .attr('x', dimensions.match_rect.x + ((over_notifier + 1) * dimensions.over_rect.width) + 80)
+            .attr('y', dimensions.match_rect.y - dimensions.match_rect.spacing)
             .text(over_notifier + 1)
             .attr("font-family", "monospace")
             .attr("font-size", "10px");
@@ -240,26 +238,25 @@ d3.json("./data/1.json").then((data) => {
 
 
         graph.append("rect")
-        .attr('x', match_rect.x)
-        .attr('y', match_rect.y + ((+match * 2)+ match_rect.offset) + (+match* match_rect.height))
-        .attr('width',match_rect.width)
-        .attr('height', match_rect.height)
-        // .attr('rx',10)
+        .attr('x', dimensions.match_rect.x)
+        .attr('y', dimensions.match_rect.y + ((+match * 2)+ dimensions.match_rect.offset) + (+match* dimensions.match_rect.height))
+        .attr('width',dimensions.match_rect.width)
+        .attr('height', dimensions.match_rect.height)
         .attr('fill', '#fff')
         .attr('stroke', '#111');
 
         graph.append("text")
             .text( +match+1)
-            .attr('x', match_rect.x + (match_rect.width/2))
-            .attr('y',match_rect.y + ((+match * 2)+ match_rect.offset) + (+match* match_rect.height) + (match_rect.height/2))
+            .attr('x', dimensions.match_rect.x + (dimensions.match_rect.width/2))
+            .attr('y', dimensions.match_rect.y + ((+match * 2)+ dimensions.match_rect.offset) + (+match* dimensions.match_rect.height) + (dimensions.match_rect.height/2))
             .attr('text-anchor', 'middle')
             .attr("font-family", "monospace")
             .attr("font-size", "10px");
 
         graph.append("text")
             .text( data[+match+1]['team1'] + " vs " + data[+match+1]['team2'])
-            .attr('x', match_rect.x + (match_rect.width/2))
-            .attr('y',match_rect.y + ((+match * 2)+ match_rect.offset) + (+match* match_rect.height) + (match_rect.height/2) + 15)
+            .attr('x', dimensions.match_rect.x + (dimensions.match_rect.width/2))
+            .attr('y',dimensions.match_rect.y + ((+match * 2)+ dimensions.match_rect.offset) + (+match* dimensions.match_rect.height) + (dimensions.match_rect.height/2) + 15)
             .attr('text-anchor', 'middle')
             .attr("font-family", "monospace")
             .attr("font-size", "10px");
@@ -273,12 +270,12 @@ d3.json("./data/1.json").then((data) => {
                     let wkt = innings_details[0]['over_by_over'][+over]['wkt'];
 
                     graph.append("rect")
-                        .attr('x', match_rect.x  - ((+over+1) * over_rect.width))
-                        .attr('y', match_rect.y + ((match * 2)+ match_rect.offset) + (match * match_rect.height))
-                        .attr('width',over_rect.width)
-                        .attr('height', match_rect.height)
-                        .attr('rx',over_rect.curve)
-                        .attr('ry',over_rect.curve)
+                        .attr('x', dimensions.match_rect.x  - ((+over+1) * dimensions.over_rect.width))
+                        .attr('y', dimensions.match_rect.y + ((match * 2)+ dimensions.match_rect.offset) + (match * dimensions.match_rect.height))
+                        .attr('width',dimensions.over_rect.width)
+                        .attr('height', dimensions.match_rect.height)
+                        .attr('rx',dimensions.over_rect.curve)
+                        .attr('ry',dimensions.over_rect.curve)
                         .attr("fill", runScoredColor(+innings_details[0]['over_by_over'][over]['total_runs']) )
                         .attr('stroke', '#111')
                         .on("mouseover", function (event, d) {
@@ -306,8 +303,8 @@ d3.json("./data/1.json").then((data) => {
                     if (innings_details[0]['over_by_over'][over]['wkt'] >0){
 
                         graph.append('circle')
-                            .attr('cx', match_rect.x  - ((+over+1) * over_rect.width) + (over_rect.width/2))
-                            .attr('cy', match_rect.y + ((match * 2)+ match_rect.offset) + (match * match_rect.height) + (match_rect.height/2))
+                            .attr('cx', dimensions.match_rect.x  - ((+over+1) * dimensions.over_rect.width) + (dimensions.over_rect.width/2))
+                            .attr('cy', dimensions.match_rect.y + ((match * 2)+ dimensions.match_rect.offset) + (match * dimensions.match_rect.height) + (dimensions.match_rect.height/2))
                             .attr('r', innings_details[0]['over_by_over'][over]['wkt'] * 3)
                             .attr('fill', 'red')
 
@@ -324,12 +321,12 @@ d3.json("./data/1.json").then((data) => {
                     let wkt = innings_details[1]['over_by_over'][+over]['wkt'];
 
                     graph.append("rect")
-                        .attr('x', match_rect.x + match_rect.width  + (+over * over_rect.width))
-                        .attr('y', match_rect.y + ((match * 2)+ match_rect.offset) + (match * match_rect.height))
-                        .attr('width',over_rect.width)
-                        .attr('height', match_rect.height)
-                        .attr('rx',over_rect.curve)
-                        .attr('ry',over_rect.curve)
+                        .attr('x', dimensions.match_rect.x + dimensions.match_rect.width  + (+over * dimensions.over_rect.width))
+                        .attr('y', dimensions.match_rect.y + ((match * 2)+ dimensions.match_rect.offset) + (match * dimensions.match_rect.height))
+                        .attr('width',dimensions.over_rect.width)
+                        .attr('height', dimensions.match_rect.height)
+                        .attr('rx',dimensions.over_rect.curve)
+                        .attr('ry',dimensions.over_rect.curve)
                         .attr("fill", runScoredColor(innings_details[1]['over_by_over'][over]['total_runs']) )
                         .attr('stroke', '#111')
                         .on("mouseover", function (event, d) {
@@ -356,8 +353,8 @@ d3.json("./data/1.json").then((data) => {
                     if (innings_details[1]['over_by_over'][over]['wkt'] >0){
 
                         graph.append('circle')
-                            .attr('cx',match_rect.x + match_rect.width  + (+over * over_rect.width) + (over_rect.width/2))
-                            .attr('cy', match_rect.y + ((match * 2)+ match_rect.offset) + (match * match_rect.height) + (match_rect.height/2))
+                            .attr('cx',dimensions.match_rect.x + dimensions.match_rect.width  + (+over * dimensions.over_rect.width) + (dimensions.over_rect.width/2))
+                            .attr('cy', dimensions.match_rect.y + ((match * 2)+ dimensions.match_rect.offset) + (match * dimensions.match_rect.height) + (dimensions.match_rect.height/2))
                             .attr('r', innings_details[1]['over_by_over'][over]['wkt'] * 3)
                             .attr('fill', 'red')
 
